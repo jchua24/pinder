85f33d2e6fdbdf6b173e4b7ebfc4282c41476173
Parsa <parsa1379.tajik@gmail.com>
2021-02-27 21:03:48 -0500

    adding more components and formatting

--- pet-adoption/src/components/Application.js
+++ pet-adoption/src/components/Application.js
@@ -1,12 +1,8 @@
 import React from "react";
 
-class Application extends React.Component{
-    constructor(props){
-
-    }
-    render() {
-        return (
-             <h1>In progress</h1>
-        );
-    }
+class Application extends React.Component {
+  constructor(props) {}
+  render() {
+    return <h1>In progress</h1>;
+  }
 }
---
+++ pet-adoption/src/components/Pet.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+class Pet extends React.Component {
+  constructor(props) {}
+  render() {
+    return <h1> In progress </h1>;
+  }
+}
