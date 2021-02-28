d04aac0da5477de17e19991b5f594373a4526b39
Parsa <parsa1379.tajik@gmail.com>
2021-02-27 21:13:21 -0500

    updated the Footer component

--- pet-adoption/src/App.js
+++ pet-adoption/src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import logo from "./logo.svg";
+import "./App.css";
+import "./components/Footer"
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +21,8 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <Footer/>
     </div>
   );
 }
--- pet-adoption/src/components/Footer.js
+++ pet-adoption/src/components/Footer.js
@@ -1,7 +1,9 @@
 const Footer = () => {
   return (
     <footer>
-      <p> Copyright &copy; 2021 </p> <a href="/about"> About </a>{" "}
+      <p> Copyright &copy; 2021 </p>
+      <p> We are the Doggo Lovers</p>
+      <a href="/about"> About </a>{" "}
     </footer>
   );
 };
