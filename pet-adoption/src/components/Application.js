7c492f06fed860b9a3655aea6c7e244021851789
Parsa <parsa1379.tajik@gmail.com>
2021-02-27 19:51:28 -0500

    added the components folder

---
+++ pet-adoption/src/components/Application.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class Application extends React.Component{
+    constructor(props){
+
+    }
+    render() {
+        return (
+             <h1>In progress</h1>
+        );
+    }
+}
