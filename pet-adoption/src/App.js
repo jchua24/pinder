8d3282b2c56e3e97189dfd12041556ab2463efcc
Parsa <parsa1379.tajik@gmail.com>
2021-03-04 22:55:41 -0500

    disabling the footer

--- pet-adoption/src/App.js
+++ pet-adoption/src/App.js
@@ -52,9 +52,9 @@ class App extends React.Component {
               </Route>
             </Switch>
           </div>
-          <div className="footerContainer">
+          {/* <div className="footerContainer">
             <Footer />
-          </div>
+          </div> */}
         </Router>
       </div>
     );
