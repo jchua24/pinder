390a838d893f1c14e2d9202e669f98eb83063180
Jimbo <jimboxie@hotmail.com>
2021-04-02 23:37:42 -0400

    Reworked user apps

--- pet-adoption/src/components/Applications/Applications.js
+++ pet-adoption/src/components/Applications/Applications.js
@@ -1,44 +1,170 @@
 import React from "react";
 import "./Applications.css";
-import Pet from "../Pet/Pet";
+import { Form, Button, Card, Row, Col } from "react-bootstrap";
+import UserApplication from "../userApplication/userApplication";
 import { uid } from 'react-uid'
 
 class Applications extends React.Component {
-  constructor(props) {
+  constructor() {
     super();
     this.state = {
-      apps: [
-        {name: "Biscuit", type: "Dog", breed: "Golden Retriever", imgSrc: "/dogo.jpeg", addInfo: "This is a lit puppy", comments: "I really like this dog!"},
-        {name: "Pussy Cat", type: "Cat", breed: "Long Haired", imgSrc: "/cat1.jpg", addInfo: "CAT", comments: "He reminds me of my current cat. He'll make a great friend!"},
-        {name: "Biscuit", type: "Dog", breed: "Golden Retriever", imgSrc: "/dogo.jpeg", addInfo: "This is a lit puppy", comments: "I really like this dog!"},
-        
-      ]
-    }
+      availPets: [],
+      userApps: [],
+      searchName: "",
+      searchType: "",
+      serachBreed: "",
+    };
   }
-  removeApp (app) {
-    const filteredApps = this.state.apps.filter((appList) =>{
-      return appList != app
-    })
-    this.setState({
-      apps: filteredApps
-    })
+
+  getAvailPets = () => {
+    // get the current pets of the current clinic from the database
+    let availPets = [
+      { name: "Biscuit", type: "Dog", breed: "Golden Retriever", img: "/dogo.jpeg" },
+      { name: "Nosey", type: "Fish", breed: "YYZ", img: "/nosey-fish.jpg" },
+      { name: "Pussy Cat", type: "Cat", breed: "Long Haired", img: "/cat1.jpg"},
+    ];
+    this.setState({ availPets: availPets }, () =>
+      console.log("available pets were acquired")
+    );
+    this.getUserApps();
+  };
+
+  getUserApps = () => {
+    // get user applications that were sent to this clinic from the database
+    let userApps = [
+      { userName: "Parsa", appliedPet: "Biscuit" },
+      { userName: "Bam", appliedPet: "Nosey" },
+      { userName: "Parsa", appliedPet: "Pussy Cat" },
+    ];
+    this.setState({ userApps: userApps }, () =>
+      console.log("user applications were acquired")
+    );
+  };
+
+  searchApps = (e) => {
+    e.preventDefault();
+    alert('The search functionality is not fully implemented at the moment.');
+  };
+
+  componentDidMount() {
+    if (this.state.availPets.length === 0) this.getAvailPets();
   }
 
   render() {
+    let { availPets, userApps } = this.state;
     return (
-      <div className = "Applications">
-        <ul className="list">
-          {
-            this.state.apps.map((app) => {
-              return(
-              <li className = "item" key={uid(app)}>
-                <Pet name={app.name} type={app.type} breed={app.breed} imgSrc={app.imgSrc} addInfo={app.addInfo} comments={app.comments} imgHeight='50%'/>
-                <button onClick={() => this.removeApp(app)}> Remove Application </button>
-              </li>
-              )
-            })
-          }
-        </ul>
+      <div>
+        {availPets.length !== 0 ? (
+          <div className="appsContainer">
+            {availPets.map((pet) => (
+              <Card className="appsCard">
+                <Card.Header>
+                  <strong>{pet.name + " - " + pet.type + " - " + pet.breed}</strong>
+                </Card.Header>
+                <ul className="list-group list-group-flush">
+                  {userApps
+                    .filter((app) => app.appliedPet === pet.name)
+                    .map((app) => (
+                      <li
+                        className="list-group-item"
+                        key={userApps.indexOf(app)}
+                      >
+                        <UserApplication
+                          admin={false}
+                          width={100}
+                          height={100}
+                          imgSrc={pet.img}
+                          userName={app.userName}
+                          appliedPet={app.appliedPet}
+                          summary="I really like this pet! Please consider my application."
+                        />
+                      </li>
+                    ))}
+                </ul>
+              </Card>
+            ))}
+          </div>
+        ) : (
+          <h3 style={{ color: "white" }}>
+            There are no pets left at the moment
+          </h3>
+        )}
+        <div className="appsSearch">
+          <Form onSubmit={this.searchApps}>
+            <Form.Group as={Row} controlId="searchName">
+              <Form.Label column sm={2}>
+                <strong>Name:</strong>{" "}
+              </Form.Label>
+              <Col>
+                <Form.Control
+                  as="select"
+                  custom
+                  onChange={(e) => {
+                    this.setState({ searchName: e.target.value });
+                  }}
+                >
+                <option selected value="">
+                  Choose..
+                </option>
+                {availPets.map(pet => (
+                  <option value={pet.name}>{pet.name}</option>
+                ))}
+                </Form.Control>
+              </Col>
+            </Form.Group>
+            <Form.Group as={Row} controlId="searchType">
+              <Form.Label column sm={2}>
+                <strong>Type:</strong>{" "}
+              </Form.Label>
+              <Col>
+                <Form.Control
+                  as="select"
+                  custom
+                  onChange={(e) => {
+                    this.setState({ searchName: e.target.value });
+                  }}
+                >
+                <option selected value="">
+                  Choose..
+                </option>
+                {availPets.map(pet => pet.type).filter((x, i, a) => a.indexOf(x) === i).map(p => (
+                  <option value={p}>{p}</option>
+                ))}
+                </Form.Control>
+              </Col>
+            </Form.Group>
+            <Form.Group as={Row} controlId="searchType">
+              <Form.Label column sm={2}>
+                <strong>Breed:</strong>{" "}
+              </Form.Label>
+              <Col>
+                <Form.Control
+                  as="select"
+                  custom
+                  onChange={(e) => {
+                    this.setState({ searchName: e.target.value });
+                  }}
+                >
+                <option selected value="">
+                  Choose..
+                </option>
+                {availPets.map(pet => pet.breed).filter((x, i, a) => a.indexOf(x) === i).map(p => (
+                  <option value={p}>{p}</option>
+                ))}
+                </Form.Control>
+              </Col>
+            </Form.Group>
+            <Button
+              block
+              size="lg"
+              type="submit"
+              style={{ backgroundColor: "#429EA6", borderColor: "transparent" }}
+              className="justify-content-md-center"
+            >
+              Search
+            </Button>
+          </Form>
+        </div>
       </div>
     );
   }
--- pet-adoption/src/components/userApplication/userApplication.js
+++ pet-adoption/src/components/userApplication/userApplication.js
@@ -2,11 +2,15 @@ import React from "react";
 import {Row, Col, Image, Button} from "react-bootstrap";
 
 class UserApplication extends React.Component{
-    constructor(){
-        super();
+    constructor(props){
+        super(props);
         console.log('the user app constructor was called');
     }
-
+    static defaultProps = {
+        admin: true,
+        width: 50,
+        height: 50
+    }
     rejectApplication = (e) => {
         e.preventDefault();
         alert("Application was rejected");
@@ -17,27 +21,50 @@ class UserApplication extends React.Component{
         alert("Application was accepted");
     }
 
+    cancelApplication = (e) => {
+        e.preventDefault();
+        alert("Successfully cancelled application")
+        console.log(this.props.width)
+    }
+
     render(){
         console.log(this.props);
         return (
             <Row>
                 <Col>
-                    <Image width="50" height="50" src={this.props.imgSrc} rounded/>
+                    <Image width={this.props.width} height={this.props.height} src={this.props.imgSrc} rounded/>
                 </Col>
                 <Col xs={6}>
-                    <div>
-                        <p>
-                            <strong>Name: </strong> {this.props.userName}
+                    {this.props.admin ? (
+                        <div>
+                            <p>
+                            <strong>Applicant Name: </strong> {this.props.userName}
                             <br />
                             <strong>Pet: </strong> {this.props.appliedPet}
                             <br />
                             <strong>Summary: </strong> {this.props.summary}
-                        </p>
+                            </p>
+                        </div>
+                    ) : (
+                        <div>
+                            <strong>Pet: </strong> {this.props.appliedPet}
+                            <br />
+                            <strong>Summary: </strong> {this.props.summary}
+                        </div>
+                    )}
+                    <div>
+                        
                     </div>
                 </Col>
                 <Col>
-                    <Button variant="success" onClick={this.rejectApplication}>Accept</Button>
-                    <Button variant="danger" onClick={this.acceptApplication}>Reject</Button>
+                    {this.props.admin ? (
+                        <div>
+                            <Button variant="success" onClick={this.acceptApplication}>Accept</Button>
+                            <Button variant="danger" onClick={this.rejectApplication}>Reject</Button>
+                        </div>
+                    ) : (
+                        <Button variant="danger" onClick={this.cancelApplication}>Cancel</Button>
+                    )}
                 </Col>
             </Row>
         );
